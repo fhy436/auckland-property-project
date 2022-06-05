530d3cfe0d2f560045430a3cef563584f16070fd
DESKTOP-IEN7UUP\64211 <frederickhy001@gmail.com>
2022-06-05 19:07:28 +1200

    Started navbar, will adjust later

---
+++ frontend/src/pages/Homepage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../components/navbar/Navbar";
+
+const Homepage = () => {
+
+    return(
+        <>
+        <Navbar/>
+        </>
+    )
+}
+
+export default Homepage;
