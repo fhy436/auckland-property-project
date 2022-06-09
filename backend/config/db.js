b154845aed6532996d5df14bf5455f33e86c2931
DESKTOP-IEN7UUP\64211 <frederickhy001@gmail.com>
2022-06-09 17:42:15 +1200

    Update app.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
+
+    const MONGO_URI = "mongodb://admin:admin@mongo:27017/properties?authSource=admin";
     try{
         const conn = await mongoose.connect(process.env.MONGO_URI)
         console.log(`MongoDB Connected: ${conn.connection.host}`);
